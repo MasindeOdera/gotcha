c0a652e6171aff0723f328d97e5db7f6186e2637
Masinde Odera <masindeodera@gmail.com>
2020-10-09 16:14:49 +0200

    Created login component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import Login from './components/login';
 import logo from './images/logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Login />
       <h3>Gotcha!</h3>
-      <img src={logo} alt="logo" width="200" height="200" />
+      <img src={logo} alt="logo" width="80" height="80" />
     </div>
   );
 }
