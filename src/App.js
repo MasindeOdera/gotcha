053aa5d5d8f1531fc7438f09d0ab0bfc0ba7ca1b
Masinde Odera <masindeodera@gmail.com>
2020-10-09 18:40:04 +0200

    Updated the login component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import Login from './components/login';
-import logo from './images/logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Login />
-      <h3>Gotcha!</h3>
-      <img src={logo} alt="logo" width="80" height="80" />
     </div>
   );
 }
--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,11 +1,36 @@
 import React from 'react';
+import logo from '../../images/logo.png';
 import './styles.css';
 
-const Login = () =>  {
+const Login = () => {
+
+    const handleChangeEvents = () => {
+    //handle change events
+    };
+
+    const handleSubmitevents = () => {
+    // handle submit events
+    };
+
+    const handlePasswordChange = () => {
+    //handle password change events
+    };
+
     return (
-        <div>
-            <h2>Login Page</h2>
-            
+        <div className="login">
+            <form onSubmit={handleSubmitevents}>
+                <img src={logo} alt="logo" width="60" height="60" />
+                <h1>Gotcha</h1>
+                <div className="email">
+                    <label>Email:</label>
+                    <input type="text" data-test="email" defaultValue="Email" onChange={handleChangeEvents} />
+                </div>
+                <div className="password">
+                    <label>Password:</label>
+                    <input type="password" data-test="password" defaultValue="Password" onChange={handlePasswordChange} />
+                </div>
+            <input type="submit" value="Login" data-test="submit" />
+            </form>
         </div>
     )
 }
