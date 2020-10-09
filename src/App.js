9974c330ffcffbd4263d59f64518a5dc2821ff41
Masinde Odera <masindeodera@gmail.com>
2020-10-09 14:44:45 +0200

    Updated Gotcha Logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import logo from './images/logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h3>Gotcha!</h3>
+      <img src={logo} alt="logo" width="200" height="200" />
     </div>
   );
 }
