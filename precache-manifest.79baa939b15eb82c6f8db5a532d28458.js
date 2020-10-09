171fe456081fcb354a8fc5ddba3e60f78fc69d63
Masinde Odera <masindeodera@gmail.com>
2020-10-09 14:54:39 +0200

    Updates

---
+++ precache-manifest.79baa939b15eb82c6f8db5a532d28458.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "714ba71737a69d46188274949046f1b8",
+    "url": "/gotcha/index.html"
+  },
+  {
+    "revision": "1f210d319eef2f4c8d19",
+    "url": "/gotcha/static/css/main.a9cd21a9.chunk.css"
+  },
+  {
+    "revision": "38759028b03a077ff1da",
+    "url": "/gotcha/static/js/2.0f394fff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gotcha/static/js/2.0f394fff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f210d319eef2f4c8d19",
+    "url": "/gotcha/static/js/main.f309d427.chunk.js"
+  },
+  {
+    "revision": "e4d6498133e76938657d",
+    "url": "/gotcha/static/js/runtime-main.6e0473d1.js"
+  },
+  {
+    "revision": "6d3018f247528742d146539013af988d",
+    "url": "/gotcha/static/media/logo.6d3018f2.png"
+  }
+]);
